7e544b50b112094ea653bef84099f39eb797ec67
algo1337 <bangod1337@gmail.com>
2024-10-10 14:49:25 +0000

    Destructor

--- array.c
+++ array.c
@@ -47,16 +47,33 @@ int Array__Remove(Array *a, int idx) {
 	if(!a || !a->arr || idx >= a->idx)
 		return 0;
 
+	void **new = (void **)malloc(sizeof(void *) * (a->idx - 1));
+	if(!new)
+		return -1;
 
-	void **new = (void **)malloc(sizeof(void *) * 1);
 	int n = 0;
-
 	for(int i = 0; i < a->idx; i++) {
 		if(i == idx)
 			continue;
 
 		new[n] = a->arr[i];
 		n++;
-		new = (void **)realloc(new, sizeof(void *) * (n + 1));
+	}
+
+	free(a->arr);
+	a->arr = new;
+	a->idx--;
+
+	return 1;
+}
+
+void DestructArray(Array *a) {
+	if(a->arr) {
+		for(int i = 0; i < a->idx; i++) {
+			if(a->arr[i]) {
+				free(a->arr[i]);
+				a->arr[i] = NULL;
+			}
+		}
 	}
 }
