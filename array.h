65350e233c74aec1bd61da127852463e96cda8e8
algo1337 <bangod1337@gmail.com>
2024-10-10 15:11:37 +0000

    Documentation Comments

--- array.h
+++ array.h
@@ -10,8 +10,31 @@ typedef struct Array {
 	void		(*Destruct)	(Array *a);
 } Array;
 
-Array NewArray(const void **arr);
-void *GetElement(Array *a, int idx);
-int Array__Append(Array *a, void *element);
-int Array__Remove(Array *a, int idx);
-void DestructArray(Array *a);
+//
+//			| - > Create a new Array intanse
+//			| - > Returns the struct with the array provided or a new array
+//
+Array 		NewArray(const void **arr);
+
+//
+//			| - > Get an element from the array with the position provided
+//			| - > Returns the element upon success or NULL upon failure
+//
+void 		*GetElement(Array *a, int idx);
+
+//
+//			| - > Append an element to the array
+//			| - > Returns 1 on success, 0 on failure and -1 for memory issue
+//
+int 		Array__Append(Array *a, void *element);
+
+//
+//			| - > Remove an element from array with position provided
+//			| - > Returns 1 upon success or 0 upon failure
+//
+int 		Array__Remove(Array *a, int idx);
+
+//
+//			| -> Destroy the array
+//
+void 		DestructArray(Array *a);
